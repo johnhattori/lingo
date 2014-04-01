d0fc3557d02ccaa7b0e37a9eb38899a869351f30
john hattori <johnmhattori@yahoo.com>
2014-03-31 21:32:01 -0700

    add tests for scoring function

--- lingo.py
+++ lingo.py
@@ -16,4 +16,26 @@ def main():
                 colored = color_score(score)
                 print(colored)
             
+def get_score(target, guess):
+    print(target, guess, end=" : ")
 
+    assert len(target) == len(guess)
+    
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    print(get_score('a', 'a'), 'A')
+    print(get_score('a', 'b'), '-') 
+    print(get_score('ab', 'ab'), 'AB')
+    print(get_score('ab', 'ba'), 'ba')
+    print(get_score('abc', 'cba'), 'cBa')
+    print(get_score('abc', 'def'), '---')
+    print(get_score('aa', 'ba'), '-A')
+    print(get_score('ab', 'aa'), 'A-')
